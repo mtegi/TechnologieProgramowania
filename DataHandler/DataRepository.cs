ee4015e5ad5c93cd5c3ce6c39cec146d9d514f28
Etahel <44235258+Etahel@users.noreply.github.com>
2019-10-20 19:57:13 +0200

    Rozbudowane DataRepository

--- DataHandler/DataRepository.cs
+++ DataHandler/DataRepository.cs
@@ -28,6 +28,9 @@ namespace DataHandler
 
         public void AddBook(Book book)
         {
+            if (_data.Books.ContainsKey(book.Id))
+                throw new ArgumentException("Książka o danym ID już znajduje się w katalogu");
+            else
             _data.Books.Add(book.Id, book);
         }
 
@@ -44,23 +47,47 @@ namespace DataHandler
         public void UpdateBook(int bookID, Book book)
         {
             //TODO: sprawdzanie czy mozna, wyjatki
+
+            if (_data.Books.ContainsKey(book.Id) && bookID!= book.Id)
+                throw new ArgumentException("Książka o danym ID już znajduje się w katalogu");
+            else
             _data.Books[bookID] = book;
+        }
+
+        public List<Book> FindBooks(string title)
+        {
+            List<Book> result = new List<Book>();
+            foreach (Book entry in _data.Books.Values)
+            {
+                if (String.Equals(entry.Title, title, StringComparison.CurrentCultureIgnoreCase))
+                    result.Add(entry);
+            }
+            return result;
         }
 
         public void DeleteBook(Book book)
         {
+
             //TODO: sprawdzanie czy mozna, wyjatki
             _data.Books.Remove(book.Id);
         }
 
+        public void DeleteBook(int bookID)
+        {
+            _data.Books.Remove(bookID);
+        }
+
         public void AddReader(Reader reader)
         {
+            if(_data.Readers.Any(i => i.Id==reader.Id))
+                throw new ArgumentException("Czytelnik o danym ID już znajduje się w wykazie");
+            else
             _data.Readers.Add(reader);
         }
 
         public Reader GetReader(int id)
         {
-            return _data.Readers[id];
+            return _data.Readers.Find(x => x.Id == id);
         }
 
         public IEnumerable<Reader> GetAllReaders()
@@ -74,10 +101,19 @@ namespace DataHandler
             _data.Readers.Remove(reader);
         }
 
+        public void DeleteReader(int readerId)
+        {
+            _data.Readers.RemoveAll(i => i.Id == readerId);
+        }
+
         public void UpdateReader(int id, Reader reader)
         {
-            //TODO: jakies sprawdzanie?
+            if (_data.Readers.Any(i => i.Id == reader.Id) && id != reader.Id)
+                throw new ArgumentException("Książka o danym ID już znajduje się w katalogu");
+            else
             _data.Readers[id] = reader;
         }
+
+      
     }
 }
