5dc0d432e230db68d7c0983a4cdd06e3073b0cbb
Etahel <44235258+Etahel@users.noreply.github.com>
2019-11-18 14:48:27 +0100

    DummyClasses, DummySerialization

---
+++ Zad2/UnitTests/DummySerializer.cs
@@ -0,0 +1,35 @@
+﻿using DummyClasses;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace UnitTests
+{
+    class DummySerializer
+    {
+        public void Serialize(List<DummyClass> dummies, Stream stream)
+        {
+            ObjectIDGenerator idGenerator = new ObjectIDGenerator();
+
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                foreach (DummyClass dummy in dummies)
+                {
+                    long dummyId = idGenerator.GetId(dummy, out bool firstTime);
+                    long dummyOtherId = idGenerator.GetId(dummy.Other, out firstTime);
+                    StringBuilder builder = new StringBuilder();
+                    builder.Append(dummyId);
+                    builder.Append(';');
+                    builder.Append(dummyOtherId);
+                    builder.Append(';');
+                    builder.Append(dummy.Id);
+                    writer.WriteLine(builder.ToString());
+                }
+                writer.WriteLine('#');
+            }
+        }
+    }
+}
+       
---
+++ Zad2/UnitTests/MultipleReferenceTest.cs
@@ -0,0 +1,33 @@
+﻿using DummyClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MultipleReferenceTest
+    {
+
+        [TestMethod]
+        public void DummyClassesTest()
+        {
+            DummyClass a = new DummyClass();
+            a.Id = 1;
+
+            DummyClass b = new DummyClass();
+            b.Id = 2;
+
+            DummyClass c = new DummyClass();
+            c.Id = 3;
+
+            a.Other = b;
+            b.Other = c;
+            c.Other = a;
+
+
+            
+        }
+    }
+}
