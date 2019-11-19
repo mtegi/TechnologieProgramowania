5c6f04f1a19e109f0bd7945211eb3a9a6551e74c
Etahel <44235258+Etahel@users.noreply.github.com>
2019-11-19 14:02:28 +0100

    Dummy serialization, deserialization, tests

--- Zad2/UnitTests/DummySerializer.cs
+++ Zad2/UnitTests/DummySerializer.cs
@@ -30,6 +30,48 @@ namespace UnitTests
                 writer.WriteLine('#');
             }
         }
+
+        public List<DummyClass> Deserialize(FileStream stream)
+        {
+            Dictionary<string, DummyClass> tmpDummies = new Dictionary<string, DummyClass>();
+            Dictionary<DummyClass, string> dummiesWithoutReference = new Dictionary<DummyClass, string>();
+            List<DummyClass> ret = new List<DummyClass>();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line;
+                while (!(line = reader.ReadLine()).StartsWith("#"))
+                {
+                    string[] dummyProps = line.Split(';');
+                    string objectId = dummyProps[0];
+                    string other = dummyProps[1];
+                    string id = dummyProps[2];
+                    if (tmpDummies.ContainsKey(objectId))
+                    {
+                        DummyClass deserializedDummy = tmpDummies[objectId];
+                        ret.Add(deserializedDummy);
+                    }
+                    else
+                    {
+                        DummyClass deserializedDummy = new DummyClass();
+                        deserializedDummy.Id = int.Parse(dummyProps[2]);
+                        if (tmpDummies.ContainsKey(other))
+                        {
+                            deserializedDummy.Other = tmpDummies[other];
+                        }
+                        else dummiesWithoutReference.Add(deserializedDummy, other);
+                        tmpDummies.Add(objectId, deserializedDummy);
+                        ret.Add(deserializedDummy);
+                    
+                    }
+                }
+            }
+            foreach(DummyClass dummy in ret)
+            {
+                if (dummy.Other == null)
+                    dummy.Other = tmpDummies[dummiesWithoutReference[dummy]];
+            }
+                    return ret;
+        }
     }
 }
        
--- Zad2/UnitTests/MultipleReferenceTest.cs
+++ Zad2/UnitTests/MultipleReferenceTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace UnitTests
@@ -25,9 +26,109 @@ namespace UnitTests
             a.Other = b;
             b.Other = c;
             c.Other = a;
+            DummySerializer serializer = new DummySerializer();
+            List<DummyClass> objects = new List<DummyClass>() { a, b, c };
+
+            Assert.AreEqual(1, a.Id);
+            Assert.AreEqual(2, b.Id);
+            Assert.AreEqual(3, c.Id);
+            Assert.ReferenceEquals(b, a.Other);
+            Assert.ReferenceEquals(c, b.Other);
+            Assert.ReferenceEquals(a, c.Other);
 
 
-            
+            using (FileStream stream = File.Open("dummies.txt", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(objects, stream);
+            }
+            List<DummyClass> deserializedDummies;
+            using (FileStream stream = File.Open("dummies.txt", FileMode.Open))
+            {
+                deserializedDummies = serializer.Deserialize(stream);
+            }
+
+            Assert.AreEqual(1, deserializedDummies[0].Id);
+            Assert.AreEqual(2, deserializedDummies[1].Id);
+            Assert.AreEqual(3, deserializedDummies[2].Id);
+            Assert.ReferenceEquals(deserializedDummies[1], deserializedDummies[0].Other);
+            Assert.ReferenceEquals(deserializedDummies[2], deserializedDummies[1].Other);
+            Assert.ReferenceEquals(deserializedDummies[0], deserializedDummies[2].Other);
+            foreach(DummyClass d in deserializedDummies)
+            {
+                Assert.AreNotEqual(null, d.Other);
+            }
         }
+
+        [TestMethod]
+        public void ReferenceTestOther()
+        {
+
+
+            DummyClass a = new DummyClass();
+            a.Id = 1;
+
+            DummyClass b = new DummyClass();
+            b.Id = 2;
+
+            DummyClass c = new DummyClass();
+            c.Id = 3;
+
+            DummyClass d = new DummyClass();
+            d.Id = 4;
+
+            DummyClass e = new DummyClass();
+            e.Id = 5;
+
+
+            a.Other = b;
+            b.Other = c;
+            c.Other = d;
+            d.Other = c;
+            e.Other = b;
+
+
+            DummySerializer serializer = new DummySerializer();
+            List<DummyClass> objects = new List<DummyClass>() { a, b, c, d, e };
+
+            Assert.AreEqual(1, a.Id);
+            Assert.AreEqual(2, b.Id);
+            Assert.AreEqual(3, c.Id);
+            Assert.AreEqual(4, d.Id);
+            Assert.AreEqual(5, e.Id);
+            Assert.ReferenceEquals(b, a.Other);
+            Assert.ReferenceEquals(c, b.Other);
+            Assert.ReferenceEquals(d, c.Other);
+            Assert.ReferenceEquals(c, d.Other);
+            Assert.ReferenceEquals(b, e.Other);
+
+            using (FileStream stream = File.Open("dummies.txt", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(objects, stream);
+            }
+            List<DummyClass> deserializedDummies;
+            using (FileStream stream = File.Open("dummies.txt", FileMode.Open))
+            {
+                deserializedDummies = serializer.Deserialize(stream);
+            }
+
+
+            Assert.AreEqual(1, deserializedDummies[0].Id);
+            Assert.AreEqual(2, deserializedDummies[1].Id);
+            Assert.AreEqual(3, deserializedDummies[2].Id);
+            Assert.AreEqual(4, deserializedDummies[3].Id);
+            Assert.AreEqual(5, deserializedDummies[4].Id);
+
+            Assert.ReferenceEquals(deserializedDummies[1], deserializedDummies[0].Other);
+            Assert.ReferenceEquals(deserializedDummies[2], deserializedDummies[1].Other);
+            Assert.ReferenceEquals(deserializedDummies[3], deserializedDummies[2].Other);
+            Assert.ReferenceEquals(deserializedDummies[2], deserializedDummies[3].Other);
+            Assert.ReferenceEquals(deserializedDummies[1], deserializedDummies[4].Other);
+
+            foreach (DummyClass dummy in deserializedDummies)
+            {
+                Assert.AreNotEqual(null, dummy.Other);
+            }
+        }
+
     }
 }
