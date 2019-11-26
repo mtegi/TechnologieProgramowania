3fa3818714f42027e9d51f73ec9094cff69cad8e
Michał <m.tegi@outlook.com>
2019-11-26 12:39:47 +0100

    formatter

---
+++ Zad2/DummyClasses/CustomFormatter.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClasses
+{
+    
+    class CustomFormatter : Formatter
+    {
+        private StreamWriter writer;
+        public override ISurrogateSelector SurrogateSelector { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override SerializationBinder Binder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override StreamingContext Context { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override object Deserialize(Stream serializationStream)
+        {
+            throw new NotImplementedException();
+        }
+        public void Serialize(Stream serializationStream, ISerializable graph)
+        {
+            var myType = graph.GetType();
+            SerializationInfo info = new SerializationInfo(myType, new FormatterConverter());
+            Context = new StreamingContext();
+            graph.GetObjectData(info, Context);
+            //TODO: i co teraz?
+        }
+        public override void Serialize(Stream serializationStream, object graph)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteArray(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteBoolean(bool val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteByte(byte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteChar(char val, string name)
+        {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(val);
+                builder.Append(';');
+        }
+
+        protected override void WriteDateTime(DateTime val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDecimal(decimal val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDouble(double val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt16(short val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt32(int val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt64(long val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteObjectRef(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteSByte(sbyte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteSingle(float val, string name)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(val);
+            builder.Append(';');
+        }
+
+        protected override void WriteTimeSpan(TimeSpan val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt16(ushort val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt32(uint val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt64(ulong val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteValueType(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Zad2/DummyClasses/DummyClass.cs
+++ Zad2/DummyClasses/DummyClass.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DummyClasses
 {
     [Serializable]
-    public class DummyClass
+    public class DummyClass:ISerializable
     {
         public DummyClass Other { get; set; }
-        public int Id { get; set; }
+        public float Id { get; set; }
 
+        public DummyClass(SerializationInfo info,StreamingContext context)
+        {
+            Id = info.GetSingle(nameof(Id));
+            Other = info.GetValue(nameof(Other), typeof(DummyClass)) as DummyClass;
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(Id), this.Id, typeof(float));
+            info.AddValue(nameof(Other), this.Other, typeof(DummyClass));
+        }
     }
 }
