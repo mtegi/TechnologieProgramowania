11ce453d1f80cf289404ebc7697fab851e54dc15
Michał <m.tegi@outlook.com>
2019-12-06 23:53:46 +0100

    mapowanie + metody rozszerzajace

---
+++ Zad3/Linq/LinqExtension.cs
@@ -0,0 +1,61 @@
+﻿using Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+   public static class LinqExtension
+    {
+    #region 4 deklaratywnie
+    public static List<Product> GetProductsWithoutCategory_D(this List<Product> products)
+    {
+        return (from product in products
+                where product.ProductSubcategory == null || product.ProductSubcategoryID == null
+                select product).ToList();
+    }
+
+    public static string AsList_D(this List<Product> products, List<ProductVendor> vendors)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        var query = (from product in products
+                      from vendor in vendors
+                      where vendor.ProductID == product.ProductID
+                      select product.Name + "-" + vendor.Vendor.Name).ToList();
+        foreach(var q in query)
+        {
+            stringBuilder.AppendLine(q);
+        }
+        return stringBuilder.ToString();
+    }
+    #endregion
+    #region 4 imperatywnie
+    public static List<Product> GetProductsWithoutCategory_I(this List<Product> products)
+    {
+        return products.Where(p => p.ProductSubcategory == null || p.ProductSubcategoryID == null).ToList();
+    }
+
+    public static List<Product> AsPage(this List<Product> products, int page, int numOfProducts)
+    {
+        if (page < 1 || numOfProducts < 0)
+        {
+            throw new ArgumentException();
+        }
+
+        return products.Skip(numOfProducts * (page - 1)).Take(numOfProducts).ToList();
+    }
+
+    public static string AsList_I(this List<Product> products, List<ProductVendor> vendors)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        var query = products.Join(vendors, product => product.ProductID, vendor => vendor.ProductID, (product, vendor) => product.Name + "-" + vendor.Vendor.Name).ToList();
+
+        foreach (var q in query)
+        {
+            stringBuilder.AppendLine(q);
+        }
+        return stringBuilder.ToString();
+    }
+    #endregion
+}
+
---
+++ Zad3/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8af06998-19a9-44b1-8734-23d90280f280")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
